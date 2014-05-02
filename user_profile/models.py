6bc3099f188960901219ca58c31e81189ec131e7
Francisco Manzano Magaña <keoh77@gmail.com>
2014-05-03 01:18:08 +0200

    Added storage management of avatar images

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,9 +1,52 @@
+from .storage import OverwriteStorage
+from cStringIO import StringIO 
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.db import models
+from PIL import Image
+import os
 
 class UserProfile(AbstractUser):
 
-	avatar = models.ImageField(upload_to='media/user_profile/', blank=True, default="media/user_profile/default.png")
+	def save_avatar(self, filename):
+
+		extension = filename[filename.rfind('.'):]
+		new_path = 'user_profile/%s-avatar%s' %(self.username, extension)
+		return new_path
+
+	avatar = models.ImageField(storage=OverwriteStorage(), upload_to=save_avatar, blank=True)
 
 	def __unicode__(self):
 		return self.username
+
+	def resize_avatar(self):
+		# code from with some changes : http://www.yilmazhuseyin.com/blog/dev/create-thumbnails-imagefield-django/
+		if not self.avatar:
+			return
+
+		try:
+			AVATAR_SIZE = settings.AVATAR_SIZE
+		except:
+			AVATAR_SIZE = (200,200)
+
+		DJANGO_TYPE = self.avatar.file.content_type 
+
+		image = Image.open(StringIO(self.avatar.read()))
+		image = image.resize(AVATAR_SIZE, Image.ANTIALIAS)
+
+		temp_handle = StringIO()
+		image.save(temp_handle, 'jpeg')
+		temp_handle.seek(0)
+
+		suf = SimpleUploadedFile(os.path.split(self.avatar.name)[-1],temp_handle.read(), content_type='image/jpeg')
+		self.avatar.save('%s.%s' %(os.path.splitext(suf.name)[0], 'jpg'),suf, save=False)
+
+	def save(self):
+		self.resize_avatar()
+		super(UserProfile, self).save()
+
+	class Meta:
+		ordering = ['username']
+		verbose_name = 'User profile'
+		verbose_name_plural = 'Users profiles'
