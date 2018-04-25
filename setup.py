a03ba7910fb15b93a3736f529e2712224276dff6
Francisco Manzano Magaña <keoh77@gmail.com>
2018-04-25 21:53:31 +0200

    Great refactor

--- setup.py
+++ setup.py
@@ -1,33 +1,49 @@
-#encoding:utf-8
-import os
-from setuptools import setup
+from codecs import open
+from os import path
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+from setuptools import find_packages, setup
 
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(path.join(here, 'VERSION'), encoding='utf-8') as f:
+    version = f.read()
+
 
 setup(
     name='django-keoh-userprofile',
-    version='0.0.4',
-    packages=['user_profile'],
+    version=version,
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     include_package_data=True,
-    license='BSD License',
-    description='A simple Django app manage user profiles',
-    long_description=README,
+    description='A simple Django app to manage user profiles',
+    long_description=long_description,
     url='https://github.com/KeoH/django-keoh-userprofile',
     author='Francisco Manzano Magaña',
     author_email='keoh77@gmail.com',
+    install_requires=['pillow'],
+    python_requires='>=3.4',
+    keywords='profile,django,user',
+    extras_require={
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
     classifiers=[
+        'Development Status :: 1 - Planning',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
     ],
+    project_urls={
+        'Bug Reports': 'https://github.com/KeoH/django-keoh-userprofile/issues',  # noqa
+        'Source': 'https://github.com/KeoH/django-keoh-userprofile/',
+    }
 )
--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,28 +1,18 @@
-#encoding:utf-8
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-from .models import UserProfile
 
-class UserProfileInline(admin.StackedInline):
-	model = UserProfile
-	can_delete = False
+class UserProfileAdmin(UserAdmin):
 
-class UserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ('avatar_admin',)
 
-	list_display = UserAdmin.list_display + ('avatar_admin',)
-	inlines = (UserProfileInline,)
+    def avatar_admin(self, obj):
+        return '<figure><img width="60px" height="60px" src="{}"></figure>'.format(obj.avatar)  # noqa
 
-	def avatar_admin(self, obj):
-		if obj.userprofile.avatar:
-			html = '<figure><img width="60px" height="60px" src="%s"></figure>' % (obj.userprofile.avatar.url)
-		else:
-			html = '<figure><img width="60px" height="60px" src="%s"></figure>' % ('/static/img/default.png')
-		return html
+    avatar_admin.allow_tags = True
+    avatar_admin.short_description = 'Avatar'
 
-	avatar_admin.allow_tags = True
-	avatar_admin.short_description = 'Avatar'
 
 admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+admin.site.register(User, UserProfileAdmin)
