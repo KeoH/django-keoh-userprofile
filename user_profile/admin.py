d8e05831d55c634ce805211b2cef4ff3ad574894
Francisco Manzano <keoh77@gmail.com>
2020-03-29 19:17:01 +0200

    Initial update

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+from django.utils.safestring import mark_safe
 
 
 class UserProfileAdmin(UserAdmin):
@@ -8,10 +9,17 @@ class UserProfileAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ('avatar_admin',)
 
     def avatar_admin(self, obj):
-        return '<figure><img width="60px" height="60px" src="{}"></figure>'.format(obj.avatar)  # noqa
+        temp = '<figure><img width="60px" height="60px" src="{url}"></figure>'
+        return mark_safe(temp.format(url=obj.avatar.url))
 
     avatar_admin.allow_tags = True
     avatar_admin.short_description = 'Avatar'
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('User Profile', {'fields': (
+            'avatar',
+        )}),
+    )
 
 
 admin.site.unregister(User)
